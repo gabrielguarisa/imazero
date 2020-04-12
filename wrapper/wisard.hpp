5155ed4647b4d5b583ee7113b9ca6ea64f87a47f
Gabriel Guarisa <gabrielguarisa@gmail.com>
2020-04-12 16:42:25 -0300

    removing memory wrapper

--- wrapper/wisard.hpp
+++ wrapper/wisard.hpp
@@ -5,30 +5,10 @@
 #include <cstdint>
 #include <vector>
 
-// #define MEMORY_SELF (static_cast<Memory *>(self))
 #define WISARD_SELF (static_cast<WiSARD *>(self))
 
 extern "C" {
 
-// // Memory
-// void *memory_create(void *const __mapping, const int length, const uint ndim) {
-//   uint32_t *const _mapping = (uint32_t *)__mapping;
-//   std::vector<uint32_t> mapping(_mapping, _mapping + length);
-//   return static_cast<void *>(new Memory(mapping, ndim));
-// }
-
-// void memory_destroy(void *self) { delete MEMORY_SELF; }
-
-// void memory_write(void *self, void *const image, uint dim) {
-//   MEMORY_SELF->write((uint8_t *)image, dim);
-// }
-
-// void memory_read(void *self, void *const image, void *const output) {
-//   std::vector<uint32_t> result = MEMORY_SELF->read((uint8_t *)image);
-//   std::copy(result.begin(), result.end(), (uint32_t *)output);
-// }
-
-// WiSARD
 void *wisard_create(void *const __mapping, const uint rows, const uint cols, const uint ndim,
                     const uint entrySize) {
   uint32_t *const _mapping = (uint32_t *)__mapping;
