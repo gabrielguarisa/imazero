bbe9c3db010f9b859ce16eae490124c78b5d1d23
Gabriel Guarisa <gabrielguarisa@gmail.com>
2020-04-13 00:09:34 -0300

    setup file configuration

--- setup.py
+++ setup.py
@@ -9,29 +9,26 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the relevant file
-# with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-#     long_description = f.read()
-#     f.close()
-
 __package_name__ = 'imazero'
 __src__ = 'wrapper/imazero.cpp'
 
-extensions = [Extension("wisard",
+ext_modules = [
+    Extension(
+        __package_name__,
         [__src__],
-        include_dirs=[],
-        language='c++' ),
+        include_dirs=["src/"],
+        language='c++'
+    ),
 ]
+
 setup(
     name=__package_name__,
     ext_package=__package_name__,
     packages=[__package_name__],
-    version="0.0.1",
     author='Gabriel Guarisa',
     description='A library of wisard with some models based on wisard',
-    # long_description=long_description,
-    # long_description_content_type="text/markdown",
-    ext_modules=extensions,
+    version='0.0.2',
+    ext_modules=ext_modules,
     zip_safe=False,
     keywords = ['wisard', 'weithgless', 'neural', 'net'],
     classifiers=[
